87936a4ebadb3e33695ea4bcd0f71f7aec15f410
Yuliya Pokhylova <i.pokhylova@exadel.com>
2013-12-16 18:01:07 +0200

    Model updated

--- src/main/java/com/exadel/expertgroup/employment/model/Skill.java
+++ src/main/java/com/exadel/expertgroup/employment/model/Skill.java
@@ -1,13 +1,21 @@
 package com.exadel.expertgroup.employment.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Version;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
 
 @Entity
 @Table(name="skills")
@@ -15,11 +23,6 @@ public class Skill implements Serializable {
 
 	private static final long serialVersionUID = 2561085458824722663L;
 	
-    public static Skill create(String name) {
-    	Skill skill = new Skill();
-    	skill.setName(name);
-        return skill;
-    }
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -27,8 +30,42 @@ public class Skill implements Serializable {
     @Column(name = "name", length = 100)
     private String name;
 
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "creation_time", nullable = false)
+    private Date creationTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "modification_time", nullable = false)
+    private Date modificationTime;
+    
+    @Version
+    private Integer version = 0;
+
 	public Skill() {
 	}
+
+    public static Skill create(String name) {
+    	Skill skill = new Skill();
+    	skill.setName(name);
+        return skill;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+    	this.modificationTime = new Date();
+    }
+    
+    @PrePersist
+    public void prePersist() {
+        Date now = new Date();
+        this.creationTime = now;
+        this.modificationTime = now;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
 
 	public Long getId() {
 		return id;
@@ -44,6 +81,30 @@ public class Skill implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Date getCreationTime() {
+		return creationTime;
+	}
+
+	public void setCreationTime(Date creationTime) {
+		this.creationTime = creationTime;
+	}
+
+	public Date getModificationTime() {
+		return modificationTime;
+	}
+
+	public void setModificationTime(Date modificationTime) {
+		this.modificationTime = modificationTime;
+	}
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
 	}
 
 	
