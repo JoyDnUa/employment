652e647a460002f51e2dbc094d84f7fea8e3ea96
pudovika <pavel.u87@gmail.com>
2013-12-18 15:49:20 +0200

    Added user page. Common things in pages moved to common.html

--- src/main/java/com/exadel/expertgroup/employment/controller/UserController.java
+++ src/main/java/com/exadel/expertgroup/employment/controller/UserController.java
@@ -1,16 +1,32 @@
 package com.exadel.expertgroup.employment.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.exadel.expertgroup.employment.service.UsersService;
 import com.exadel.expertgroup.employment.web.ViewUtil;
 
 @Controller
 @RequestMapping(ViewUtil.USER)
+@SessionAttributes("currentUser")
 public class UserController {
 
 	@Autowired
 	protected UsersService usersService;
+	
+	@ModelAttribute("currentUser")
+    public String currentUser(Principal principal){
+        return principal.getName();
+    }
+	
+	@RequestMapping(method = RequestMethod.GET)
+    public String user() {
+        return ViewUtil.USER;
+    }
 }
